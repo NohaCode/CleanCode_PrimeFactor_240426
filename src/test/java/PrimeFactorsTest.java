39b5578411abde95c49a70364a1c46bd4b90811e
jinha <gammazx@naver.com>
2024-04-26 16:59:08 +0900

    first commit

---
+++ src/test/java/PrimeFactorsTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PrimeFactorsTest {
+
+    @Test
+    void testPrimeFactorOf1() {
+        PrimeFactor primeFactor = new PrimeFactor();
+        assertEquals(10, 10);
+
+    }
+}
